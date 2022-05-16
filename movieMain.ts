c09d44b6b8a861957d03e6f946f54055ebd4f9f3
Claudia Diez <claudia_diez@outlook.com>
2022-05-16 17:23:17 +0200

    inclusion clase Movie

---
+++ movieMain.ts
@@ -0,0 +1,10 @@
+import { Professional } from "./professional"
+import { Movie } from "./Movie"
+
+let peliculaUNO = new Movie("Erase una vez en America",1984,"American","Action")
+let peliculaDOS = new Movie("Trono de sangre",1957,"Japon","Belica")
+let peliculaTRES =new Movie("El señor de los anillos",2001,"American","Fantasy")
+
+peliculaUNO.datospeliculas();
+peliculaDOS.datospeliculas();
+peliculaTRES.datospeliculas();
